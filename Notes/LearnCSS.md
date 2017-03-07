6f187c058070219d58aaf60f080a93e32bd69395
Carmen To <carmen_to@ymail.com>
2017-03-06 18:14:14 -0800

    game update

--- Notes/LearnCSS.md
+++ Notes/LearnCSS.md
@@ -2,8 +2,8 @@
 # CSS
 Cascading Style Sheets
 - language used to describe the appearance and formatting of your HTML
-- style sheet is a file that describe how a HTML file should look
-- style sheets are cascading bc the sheets can apply formatting when more than one style applies
+- a .css file describe how a HTML file should look
+- a .css file is a cascade bc the sheets can apply formatting when more than one style applies
 - by separating your form/formatting CSS from your functional contents/structure HTML
   - you can apply same formatting to several HTML elements
   - you can apply similar appearance and formatting to several HTML pages form a single CSS file
@@ -18,9 +18,12 @@ Cascading Style Sheets
 		<title>Result</title>
 </head>
 ```
-
-``` CSS
-span {
-	font-family: cursive;
-}
+# Linking css file in html
+``` html
+<head>
+  <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+  <title>Result</title>
+</head>
 ```
+
+##
--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,23 +1,73 @@
+let turnNumber = 1;
+let board = []
+for (let i = 0; i < 3; i += 1) {
+  let row = [];
+  for (let j = 0; j < 3; j += 1) {
+      // zero represents empty spot
+    row.push(0);
+  }
+  board.push(row);
+}
+console.table(board);
+
 const log = document.getElementById("log")
 
-let turnNumber = 1;
-
-
-for ( let num = 1; num < 10; num += 1) {
-  const box = document.getElementById(num.toString());
-  debugger
-  box.addEventListener("click", function() {
-
-    if (turnNumber % 2 === 1) {
-      box.innerHTML = 'x';
-    } else {
-      box.innerHTML = 'o';
+function isGameOver(turn) {
+    if (turn > 9) {
+      return true;
     }
-    turnnumber += 1;
-    
-    log.innerHTML = "Box " + num  + " is clicked!";
-  });
+    // check whether it's 0 wins or x wins, if neither , return false
 }
+
+function removeAllListerners() {
+  for (let num = 1; num < 10; num +=1) {
+      const box = document.getElementById(num.toString());
+      box.addEventListener("click", clickHandler)
+  }
+}
+
+function addAllListener() {
+  for (let num = 1; num < 10; num +=1) {
+      const box = document.getElementById(num.toString());
+      box.addEventListener("click", clickHandler)
+  }
+}
+
+// 0, but not 1
+// row = (box num - 1) / 3
+// col = (box num -1) % 3
+
+function clickHandler(event) {
+  // box id is a string not a number so we need to covert it
+  const box = parseInt(target.id);
+  const row = parseInt((boxNum - 1) / 3);
+  const col = parseInt((boxNum - 1) % 3);
+
+  if (turnNumber % 2 === 1) {
+    box.innerHTML = 'x';
+    board[row][col] = 'x';
+  } else {
+    box.innerHTML = 'o';
+    board[row][col] = 'o';
+  }
+
+  console.table(board);
+
+  turnNumber += 1;
+
+  if (isGameOver(turnNumber)) {
+    removeAllListerners();
+    log.innerHTML = 'Game is over!';
+  } else {
+    log.innerHTML = "Box " + num  + " is clicked!";
+  }
+}
+
+addAllListener()
+
+
+
+
 /*
  const box1 = document.getElementById("1")
 
