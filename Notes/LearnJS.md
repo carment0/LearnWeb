a893ca0e44042a6b9f632eb0318cd21f471a4843
Carmen To <carmen_to@ymail.com>
2017-03-20 23:20:29 -0700

    updated Tic Tac Toe

--- Notes/LearnJS.md
+++ Notes/LearnJS.md
@@ -305,6 +305,7 @@ var family = new Array();
 family[0] = new Person("alice", 40);
 family[1] = new Person("bob", 42);
 family[2] = new Person("michelle", 8);
+
 ```
 - accessing properties
 ``` js
--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,106 +1,186 @@
-// board = [[0, 0, 0] [0, 0, 0] [0, 0, 0]]
-let board = []
-for (let i = 0; i < 3; i += 1) {
-  let row = [];
-  for (let j = 0; j < 3; j += 1) {
-    row.push();
-  }
-  board.push(row);
-}
-console.table(board);
-
-//
-// let column = []
-// let j = 0
-// for (let i = 0; i < 3; i += 1) {
-//   column.push(board[i][j])
-// }
-
-
-// stops clicking
 function removeAllListerners() {
-  for (let num = 1; num < 10; num +=1) {
-      const box = document.getElementById(num.toString());
-      box.addEventListener("click", clickHandler)
-  }
+    for (let num = 1; num < 10; num +=1) {
+        const box = document.getElementById(num.toString());
+        box.addEventListener("click", handleClick);
+    }
 }
 
-// allows clicking
 function addAllListener() {
-  for (let num = 1; num < 10; num +=1) {
-      const box = document.getElementById(num.toString());
-      box.addEventListener("click", clickHandler)
-  }
+    for (let num = 1; num < 10; num +=1) {
+        const box = document.getElementById(num.toString());
+        box.addEventListener("click", handleClick);
+    }
 }
 
-function isRowWinningForX (row) {
-  for (let i = 0; i < 3; i++) {
-    if (row[i] !== 'x') {
-      return false;
-    }
-  }
-  return true;
-}
-
-function isRowWinningForO (row) {
-  for (let i = 0; i < 3; i++) {
-    if (row[i] !== 'o') {
-      return false;
-    }
-  }
-  return true;
-}
-
-// end game
-function isGameOver(turn) {
-    if (turn > 9) {
-      return true;
-    }
-
+function isRowWinningForX () {
     for (let rowNum = 0; rowNum < 3; rowNum++) {
-        let currentRow = board[rowNum];
+        let xCount = 0;
 
-        if (isRowWinningForX(currentRow) || isRowWinningForO(currentRow)) {
-          return true;
+        for (let i = 0; i < 3; i++) {
+
+            if (board[rowNum][i] === 'x') {
+                xCount += 1;
+            }
+
+            if (xCount === 3) {
+                return true;
+            }
         }
     }
     return false;
-    // check whether it's 0 wins or x wins, if neither , return false
+}
+
+function isColumnWinningForX () {
+  for (let colNum = 0; colNum < 3; colNum++) {
+    let xCount = 0;
+
+    for (let i = 0; i < 3; i++) {
+
+      if (board[i][colNum] === 'x') {
+        xCount += 1;
+      }
+      if (xCount === 3) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function isDiagonal1WinningForX () {
+  let xCount = 0;
+  for (let i = 0; i < 3; i++) {
+    if (board[i][i] === 'x') {
+     xCount += 1;
+   }
+   if (xCount === 3) {
+     return true;
+   }
+ }
+ return false;
+}
+
+function isDiagonal2WinningForX () {
+let xCount = 0;
+let i = 0;
+let j = 2;
+
+  while (i < 3 && j >= 0) {
+    if (board[i][j] === 'x') {
+      xCount += 1;
+      i += 1;
+      j -= 1;
+    }
+    if (xCount === 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+
+
+function isRowWinningForO () {
+    for (let rowNum = 0; rowNum < 3; rowNum++) {
+        let oCount = 0;
+
+        for (let i = 0; i < 3; i++) {
+
+            if (board[rowNum][i] === 'o') {
+                oCount += 1;
+            }
+
+            if (oCount === 3) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+function isColumnWinningForO () {
+  for (let colNum = 0; colNum < 3; colNum++) {
+    let oCount = 0;
+
+    for (let i = 0; i < 3; i++) {
+
+      if (board[i][colNum] === 'o') {
+        oCount += 1;
+      }
+      if (oCount === 3) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function isDiagonal1WinningForO () {
+ let oCount = 0;
+ for (let i = 0; i < 3; i++) {
+
+   if (board[i][i] === 'o') {
+     oCount += 1;
+   }
+   if (oCount === 3) {
+     return true;
+   }
+ }
+ return false;
+}
+
+function isGameOver() {
+    if (turnNumber > 9) {
+        return true;
+    }
+
+    if (isRowWinningForX() || isRowWinningForO() || isColumnWinningForO() || isColumnWinningForX() || isDiagonal1WinningForX() || isDiagonal2WinningForX()) {
+        return true;
+    }
+    return false;
+}
+
+function handleClick(event) {
+    const box = event.target;
+    const boxNum = parseInt(box.id);
+    const row = parseInt((boxNum - 1) / 3);
+    const col = parseInt((boxNum - 1) % 3);
+
+    if (turnNumber % 2 === 1) {
+        box.innerHTML = 'x';
+        board[row][col] = 'x';
+    } else {
+        box.innerHTML = 'o';
+        board[row][col] = 'o';
+    }
+
+    turnNumber += 1;
+    console.table(board);
+    console.log('Checking if it works', isGameOver());
+    if (isGameOver()) {
+        removeAllListerners();
+        log.innerHTML += 'Game is over!';
+    } else {
+        log.innerHTML = 'Box ' + event.target.id + ' is clicked!';
+    }
 }
 
 
 let turnNumber = 1;
-//
-function clickHandler(event) {
-  // 0, but not 1
-  // row = (box num - 1) / 3
-  // col = (box num -1) % 3
-  const box = event.target
-  const boxNum = parseInt(event.target.id);
-  const row = parseInt((boxNum - 1) / 3);
-  const col = parseInt((boxNum - 1) % 3);
 
-
-  if (turnNumber % 2 === 1) {
-    box.innerHTML = 'x';
-    board[row][col] = 'x';
-  } else {
-    box.innerHTML = 'o';
-    board[row][col] = 'o';
-  }
-
-  console.table(board);
-
-  turnNumber += 1;
-
-  if (isGameOver(turnNumber)) {
-    removeAllListerners();
-    log.innerHTML = 'Game is over!';
-  } else {
-    log.innerHTML = "Box " + boxNum  + " is clicked!";
-  }
+let board = [];
+for (let i = 0; i < 3; i += 1) {
+    let row = [];
+    for (let j = 0; j < 3; j += 1) {
+        row.push(' ');
+    }
+    board.push(row);
 }
 
-const log = document.getElementById("log")
+console.table(board);
 
-addAllListener()
+const log = document.getElementById("log");
+
+addAllListener();
